1a29ea2dbc2736d76220bf90ef0b50576d7ef2bc
mkronschnabl <matthias.kronschnabl@actano.de>
2016-11-15 14:19:59 +0100

    Don't overwrite headers subobject in option object

--- auth-fetch.js
+++ auth-fetch.js
@@ -2,11 +2,7 @@ const fetch = require('node-fetch')
 const basicAuth = require('basic-auth-header')
 
 module.exports = (url, option = {}) => {
-  const authHeader = {
-    headers: {
-      Authorization: basicAuth(option.user, option.password)
-    }
-  }
-  const authOptions = Object.assign(option, authHeader)
-  return fetch(url, authOptions)
+  if (!option.headers) option.headers = {}
+  option.headers['Authorization'] = basicAuth(option.user, option.password)
+  return fetch(url, option)
 }
